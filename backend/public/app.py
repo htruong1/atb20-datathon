4c51a483b96bec77faed8eabcc96b3aed188151c
htruong1 <htruong1@ualberta.ca>
2020-02-08 10:29:34 -0700

    pulling info for customers

--- backend/public/app.py
+++ backend/public/app.py
@@ -1,18 +1,26 @@
 from flask import Flask
 from flask_restplus import Resource, Api
-import requests
-import os
+# import requests
+# import os
+from utils.requestutil import atbRequests
+
 app = Flask(__name__)
 api = Api(app)
 
 @api.route('/api/hello')
 class HelloWorld(Resource):
     def get(self):
-        token = os.getenv('ATB_TOKEN')
-        test = { 'Authorization' : 'DirectLogin token=' }
-        data = requests.get("https://api.leapos.ca/obp/v4.0.0/banks/3621ab3c23c3b1425fb18ee80a6a7ed/atms", headers=test)
-        return {'lil_shit': data.content}
-        # return 'hello world'
+        atb_requests = atbRequests()
+        response = atb_requests.atbGet("https://api.leapos.ca/obp/v4.0.0/banks/3621ab3c23c3b1425fb18ee80a6a7ed/atms")
+        return response
+
+@api.route('/api/bank/accounts')
+class GetCustomers(Resource):
+    def get(self):
+        atb_requests = atbRequests()
+        response = atb_requests.atbGet("https://api.leapos.ca/obp/v4.0.0/banks/3621ab3c23c3b1425fb18ee80a6a7ed/accounts" )
+        return response
+
 
 if __name__ == '__main__':
     app.run(debug=True)
